8515e5cbe9c68249aa1487a3e18a9677c60b8ece
= <2709400@gmail.com>
2024-09-23 10:38:34 +0300

    draw message

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ COLORS = {
     2048 : (237, 194, 46)
 }
 
-SIZE = 4
+SIZE = 2
 TILE_SIZE = 100
 TILE_MARGIN = 10
+MAX_VALUE = 2048
 WIDTH = SIZE * TILE_SIZE + (SIZE + 1) * TILE_MARGIN
 HEIGHT = WIDTH
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -55,6 +56,9 @@ def add_new_tile(board):
         row, col = random.choice(empty_tiles)
         board[row][col] = 2 if random.random() < 0.9 else 4
 
+def rotate_board(board):
+    return [[board[col][row] for col in range(SIZE)] for row in range(SIZE - 1, -1, -1)]
+
 def move_left(board):
     new_board = [[0] * SIZE for _ in range(SIZE)]
     for row in range(SIZE):
@@ -76,6 +80,31 @@ def move_left(board):
             new_board[row][col_new] = last
     return new_board
 
+def game_over(board):
+    for row in range(SIZE):
+        for col in range(SIZE):
+            if board[row][col] == 0:
+                return False
+            if board[row][col] == 2048:
+                return True
+            if col < SIZE - 1 and board[row][col] == board[row][col + 1]:
+                return False
+            if row < SIZE - 1 and board[row][col] == board[row + 1][col]:
+                return False
+    return True
+
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, True, color)
+    textrect = textobj.get_rect()
+    textrect.center = (x, y)
+    surface.blit(textobj, textrect)
+
+def display_game_over_message():
+    draw_text("Game over!", font, WHITE, screen, WIDTH // 2, HEIGHT // 2)
+    pygame.display.flip()
+    pygame.time.wait(2000)
+
+
 def main():
     board = [[0] * SIZE for _ in range(SIZE)]
     running = True
@@ -89,10 +118,33 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key in (pygame.K_LEFT, pygame.K_a):
                     board = move_left(board)
-                    add_new_tile(board)
-            else:
-                continue
+                elif event.key in (pygame.K_RIGHT, pygame.K_d):
+                    board = rotate_board(board)
+                    board = rotate_board(board)
+                    board = move_left(board)
+                    board = rotate_board(board)
+                    board = rotate_board(board)
+                elif event.key in (pygame.K_UP, pygame.K_w):
+                    board = rotate_board(board)
+                    board = move_left(board)
+                    board = rotate_board(board)
+                    board = rotate_board(board)
+                    board = rotate_board(board)
+                elif event.key in (pygame.K_DOWN, pygame.K_s):
+                    board = rotate_board(board)
+                    board = rotate_board(board)
+                    board = rotate_board(board)
+                    board = move_left(board)
+                    board = rotate_board(board)
+                add_new_tile(board)
+                if game_over(board):
+                    display_game_over_message()
+                    running = False
+        draw_board(board)
     pygame.quit()
     sys.exit()
+
+
 if __name__ == "__main__":
     main()
+    
