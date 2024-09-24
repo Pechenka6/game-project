40e9bc87c46291d8c662e7f9dc0c256fc31ba470
= <2709400@gmail.com>
2024-09-24 13:05:41 +0300

    pre-reliz version

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ pygame.init()
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREY = (187, 173, 160)
+LIGHT_GREY = (200, 200, 200)
 COLORS = {
     2 : (238, 228, 218),
     4 : (237, 224, 200),
@@ -20,8 +21,13 @@ COLORS = {
     1024 : (237, 197, 63),
     2048 : (237, 194, 46)
 }
+DIFFICULTY_OPTIONS = {
+    "Easy": 1,
+    "Medium": 1.5,
+    "Hard": 2
+}
 
-SIZE = 2
+SIZE = 4
 TILE_SIZE = 100
 TILE_MARGIN = 10
 MAX_VALUE = 2048
@@ -80,12 +86,12 @@ def move_left(board):
             new_board[row][col_new] = last
     return new_board
 
-def game_over(board):
+def game_over(board, size):
     for row in range(SIZE):
         for col in range(SIZE):
             if board[row][col] == 0:
                 return False
-            if board[row][col] == 2048:
+            if board[row][col] == 2048 * (2 ** (size - 1)) ** 4:
                 return True
             if col < SIZE - 1 and board[row][col] == board[row][col + 1]:
                 return False
@@ -104,18 +110,144 @@ def display_game_over_message():
     pygame.display.flip()
     pygame.time.wait(2000)
 
+def draw_button(text, x, y, width, height, color, hover_color):
+    mouse = pygame.mouse.get_pos()
+    if x < mouse[0] < x + width and y < mouse[1] < y + height:
+        pygame.draw.rect(screen, hover_color, (x, y, width, height))
+    else:
+        pygame.draw.rect(screen, color, (x, y, width, height))
+    draw_text(text, font, BLACK, screen, x + width // 2, y + height // 2)
+    
+def main_menu():
+    while True:
+        screen.fill(GREY)
+        draw_text("Main Menu", font, WHITE, screen, WIDTH // 2, HEIGHT // 4)
 
-def main():
-    board = [[0] * SIZE for _ in range(SIZE)]
+        draw_button("Play", 125, 150, 200, 50, LIGHT_GREY, WHITE)
+        draw_button("Settings", 125, 220, 200, 50, LIGHT_GREY, WHITE)
+        draw_button("Exit", 125, 290, 200, 50, LIGHT_GREY, WHITE)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if 125 <= event.pos[0] <= 325:
+                    if 150 <= event.pos[1] <= 200:
+                        difficulty_menu()
+                    elif 220 <= event.pos[1] <= 270:
+                        settings_menu()
+                    elif 290 <= event.pos[1] <= 340:
+                        pygame.quit()
+                        sys.exit()
+
+        pygame.display.flip()
+
+def difficulty_menu():
+    while True:
+        screen.fill(GREY)
+        draw_text("Select Difficulty", font, WHITE, screen, WIDTH // 2, HEIGHT // 4)
+
+        y_position = 150
+        for difficulty in DIFFICULTY_OPTIONS.keys():
+            draw_button(difficulty, 125, y_position, 200, 50, LIGHT_GREY, WHITE)
+            y_position += 70
+
+        draw_button("Back", 125, y_position, 200, 50, LIGHT_GREY, WHITE)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if 125 <= event.pos[0] <= 325:
+                    if 150 <= event.pos[1] <= 200:
+                        start_game(DIFFICULTY_OPTIONS["Easy"])
+                    elif 220 <= event.pos[1] <= 270:
+                        start_game(DIFFICULTY_OPTIONS["Medium"])
+                    elif 290 <= event.pos[1] <= 340:
+                        start_game(DIFFICULTY_OPTIONS["Hard"])
+                    elif y_position <= event.pos[1] <= y_position + 50:
+                        return  
+
+        pygame.display.flip()
+
+def settings_menu():
+    while True:
+        screen.fill(GREY)
+        draw_text("Settings", font, WHITE, screen, WIDTH // 2, HEIGHT // 4)
+
+        draw_button("Back", 125, 150, 200, 50, LIGHT_GREY, WHITE)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if 100 <= event.pos[0] <= 300 and 150 <= event.pos[1] <= 200:
+                    return 
+
+        pygame.display.flip()
+
+def pause_menu():
+    while True:
+        screen.fill(GREY)
+        draw_text("Paused", font, WHITE, screen, WIDTH // 2, 50)
+
+        button_height = 50
+        button_spacing = 20
+        total_height = (button_height * 3) + (button_spacing * 2)  
+        start_y = (HEIGHT - total_height) // 2 
+
+        draw_button("Resume", 125, 80, 200, 50, LIGHT_GREY, WHITE)
+        draw_button("Settings", 125, 150, 200, 50, LIGHT_GREY, WHITE)
+        draw_button("Main menu", 125, 220, 200, 50, LIGHT_GREY, WHITE)
+        draw_button("Exit", 125, 290, 200, 50, LIGHT_GREY, WHITE)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if 125 <= event.pos[0] <= 325:
+                    if 80 <= event.pos[1] <= 130:
+                        return
+                    elif 150 <= event.pos[1] <= 200:
+                        settings_menu()
+                    elif 220 <= event.pos[1] <= 270:
+                        return main_menu()
+                    elif 290 <= event.pos[1] <= 340:
+                        pygame.quit()
+                        sys.exit()
+        pygame.display.flip()
+
+
+
+def start_game(size):
+    
+    board = [[0] * int(SIZE * size) for _ in range(int(SIZE * size))]
     running = True
+    game_paused = False
     add_new_tile(board)
     add_new_tile(board)
     while running:
+        if game_paused:
+            pause_menu()
+            game_paused = False 
+
+        screen.fill(GREY)
+        draw_text("Game Running", font, WHITE, screen, WIDTH // 2, HEIGHT // 4)  
         draw_board(board)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:  
+                    game_paused = True
                 if event.key in (pygame.K_LEFT, pygame.K_a):
                     board = move_left(board)
                 elif event.key in (pygame.K_RIGHT, pygame.K_d):
@@ -137,7 +269,7 @@ def main():
                     board = move_left(board)
                     board = rotate_board(board)
                 add_new_tile(board)
-                if game_over(board):
+                if game_over(board, size):
                     display_game_over_message()
                     running = False
         draw_board(board)
@@ -146,5 +278,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main_menu()
     
